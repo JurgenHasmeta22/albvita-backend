4429aa064ae5ece6366d6e0c5d7ece977872f96b
DESKTOP-OU311RG\User <jurgenhasmeta12@yahoo.com>
2022-08-27 14:55:55 +0200

    new stuff

--- index.ts
+++ index.ts
@@ -1,101 +1,101 @@
 // #region 'Importing and configuration of Prisma'
-import express from 'express';
-import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const prisma = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
+  log: ["query", "info", "warn", "error"],
 });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', async (req, res) => {
-    res.send('Server Up and Running');
+app.get("/", async (req, res) => {
+  res.send("Server Up and Running");
 });
 
 app.listen(4000, () => {
-    console.log(`Server up: http://localhost:4000`);
+  console.log(`Server up: http://localhost:4000`);
 });
 // #endregion
 
 // #region "Token, and getting user loggied in, register, validating if user is logged in"
 function createToken(id: number) {
-    //@ts-ignore
-    const token = jwt.sign({ id: id }, process.env.MY_SECRET, {
-        expiresIn: '3days',
-    });
+  //@ts-ignore
+  const token = jwt.sign({ id: id }, process.env.MY_SECRET, {
+    expiresIn: "3days",
+  });
 
-    return token;
+  return token;
 }
 
 async function getUserFromToken(token: string) {
-    //@ts-ignore
-    const data = jwt.verify(token, process.env.MY_SECRET);
+  //@ts-ignore
+  const data = jwt.verify(token, process.env.MY_SECRET);
 
-    const user = await prisma.user.findUnique({
-        // @ts-ignore
-        where: { id: data.id },
-    });
+  const user = await prisma.user.findUnique({
+    // @ts-ignore
+    where: { id: data.id },
+  });
 
-    return user;
+  return user;
 }
 
-app.post('/sign-up', async (req, res) => {
-    const { email, password, userName } = req.body;
+app.post("/sign-up", async (req, res) => {
+  const { email, password, userName } = req.body;
 
-    try {
-        const hash = bcrypt.hashSync(password);
+  try {
+    const hash = bcrypt.hashSync(password);
 
-        const user = await prisma.user.create({
-            data: { email, password: hash, userName },
-        });
+    const user = await prisma.user.create({
+      data: { email, password: hash, userName },
+    });
 
-        res.send({ user, token: createToken(user.id) });
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+    res.send({ user, token: createToken(user.id) });
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
-        const user = await prisma.user.findUnique({
-            where: { email },
-        });
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-        // @ts-ignore
-        const passwordMatches = bcrypt.compareSync(password, user.password);
+    // @ts-ignore
+    const passwordMatches = bcrypt.compareSync(password, user.password);
 
-        if (user && passwordMatches) {
-            res.send({ user, token: createToken(user.id) });
-        } else {
-            throw Error('Boom');
-        }
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
+    if (user && passwordMatches) {
+      res.send({ user, token: createToken(user.id) });
+    } else {
+      throw Error("Boom");
     }
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/validate', async (req, res) => {
-    const token = req.headers.authorization || '';
+app.get("/validate", async (req, res) => {
+  const token = req.headers.authorization || "";
 
-    try {
-        const user = await getUserFromToken(token);
-        res.send(user);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+  try {
+    const user = await getUserFromToken(token);
+    res.send(user);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 // #endregion
 
@@ -103,102 +103,156 @@ app.get('/validate', async (req, res) => {
 
 // #region "Products REST API Endpoints"
 
-app.get('/products/page/:pagenr', async (req, res) => {
-    const sortBy = req.query.sortBy;
-    const ascOrDesc = req.query.ascOrDesc;
+app.get("/products/page/:pagenr", async (req, res) => {
+  const sortBy = req.query.sortBy;
+  const ascOrDesc = req.query.ascOrDesc;
+  const categoryName = req.query.category;
 
-    const page = Number(req.params.pagenr);
-    const nrToSkip = (page - 1) * 20;
+  const page = Number(req.params.pagenr);
+  const nrToSkip = (page - 1) * 20;
 
-    try {
-        const products = await prisma.product.findMany({
-            //@ts-ignore
-            include: { category: true },
+  try {
+    let category;
+    let products;
+    category = await prisma.category.findFirst({
+      where: { name: String(categoryName) },
+    });
+    console.log(category, categoryName, ascOrDesc, sortBy);
 
-            orderBy: {
-                //@ts-ignore
-                [sortBy]: ascOrDesc,
-            },
+    if (categoryName !== "all" && sortBy && ascOrDesc) {
+      products = await prisma.product.findMany({
+        where: { categoryId: category?.id },
+        //@ts-ignore
+        include: { category: true },
 
-            skip: nrToSkip,
-            take: 20,
-        });
+        orderBy: {
+          //@ts-ignore
+          [sortBy]: ascOrDesc,
+        },
 
-        res.send(products);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
+        skip: nrToSkip,
+        take: 20,
+      });
+    } else if (
+      categoryName !== "all" &&
+      sortBy === undefined &&
+      ascOrDesc === undefined
+    ) {
+      products = await prisma.product.findMany({
+        where: { categoryId: category?.id },
+        //@ts-ignore
+        include: { category: true },
+
+        orderBy: {
+          //@ts-ignore
+          [sortBy]: ascOrDesc,
+        },
+
+        skip: nrToSkip,
+        take: 20,
+      });
+    } else if (categoryName === "all" && sortBy && ascOrDesc) {
+      products = await prisma.product.findMany({
+        //@ts-ignore
+        include: { category: true },
+
+        orderBy: {
+          //@ts-ignore
+          [sortBy]: ascOrDesc,
+        },
+
+        skip: nrToSkip,
+        take: 20,
+      });
+    } else if (
+      categoryName === "all" &&
+      sortBy === undefined &&
+      ascOrDesc === undefined
+    ) {
+      products = await prisma.product.findMany({
+        //@ts-ignore
+        include: { category: true },
+
+        skip: nrToSkip,
+        take: 20,
+      });
     }
+
+    res.send(products);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/products/:name', async (req, res) => {
-    const name = req.params.name
-        .split('')
-        .map((char) => (char === '-' ? ' ' : char))
-        .join('');
+app.get("/products/:name", async (req, res) => {
+  const name = req.params.name
+    .split("")
+    .map((char) => (char === "-" ? " " : char))
+    .join("");
 
-    try {
-        const product = await prisma.product.findFirst({
-            where: { name },
-            //@ts-ignore
-            include: { category: true },
-        });
+  try {
+    const product = await prisma.product.findFirst({
+      where: { name },
+      //@ts-ignore
+      include: { category: true },
+    });
 
-        res.send(product);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+    res.send(product);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/products', async (req, res) => {
-    try {
-        const products = await prisma.product.findMany({
-            // @ts-ignore
-            include: { category: true },
-        });
-        res.send(products);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+app.get("/products", async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      // @ts-ignore
+      include: { category: true },
+    });
+    res.send(products);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/productsCount', async (req, res) => {
-    try {
-        const count = await prisma.product.count();
-        res.send({ count });
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+app.get("/productsCount", async (req, res) => {
+  try {
+    const count = await prisma.product.count();
+    res.send({ count });
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.post('/searchProductsByName', async (req, res) => {
-    const { name, page } = req.body;
+app.post("/searchProductsByName", async (req, res) => {
+  const { name, page } = req.body;
 
-    try {
-        const products = await prisma.product.findMany({
-            where: {
-                name: { contains: name },
-            },
-            // @ts-ignore
-            include: { category: true },
-            skip: (page - 1) * 20,
-            take: 20,
-        });
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        name: { contains: name },
+      },
+      // @ts-ignore
+      include: { category: true },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
 
-        const count = await prisma.product.count({
-            where: {
-                name: { contains: name },
-            },
-        });
+    const count = await prisma.product.count({
+      where: {
+        name: { contains: name },
+      },
+    });
 
-        res.send({ products, count });
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+    res.send({ products, count });
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
 // app.get("/favorites", async (req, res) => {
@@ -254,104 +308,104 @@ app.post('/searchProductsByName', async (req, res) => {
 
 // #region "OTHER REST API Endpoints"
 
-app.get('/users', async (req, res) => {
-    try {
-        const users = await prisma.user.findMany();
-        res.send(users);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.send(users);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/categories', async (req, res) => {
-    try {
-        const categories = await prisma.category.findMany({
-            // @ts-ignore
-            include: { products: true },
-        });
-        res.send(categories);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+app.get("/categories", async (req, res) => {
+  try {
+    const categories = await prisma.category.findMany({
+      // @ts-ignore
+      include: { products: true },
+    });
+    res.send(categories);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/orders', async (req, res) => {
-    try {
-        const orders = await prisma.order.findMany({
-            // @ts-ignore
-            include: { product: true, user: true },
-        });
-        res.send(orders);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+app.get("/orders", async (req, res) => {
+  try {
+    const orders = await prisma.order.findMany({
+      // @ts-ignore
+      include: { product: true, user: true },
+    });
+    res.send(orders);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/orders/:id', async (req, res) => {
-    const id = Number(req.params.id)
+app.get("/orders/:id", async (req, res) => {
+  const id = Number(req.params.id);
 
-    try {
-        const order = await prisma.category.findFirst({
-            where: { id },
-            //@ts-ignore
-            include: { product: true, user: true },
-        });
+  try {
+    const order = await prisma.category.findFirst({
+      where: { id },
+      //@ts-ignore
+      include: { product: true, user: true },
+    });
 
-        res.send(order);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+    res.send(order);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.get('/categories/:name', async (req, res) => {
-    const name = req.params.name
-        .split('')
-        .map((char) => (char === '-' ? ' ' : char))
-        .join('');
+app.get("/categories/:name", async (req, res) => {
+  const name = req.params.name
+    .split("")
+    .map((char) => (char === "-" ? " " : char))
+    .join("");
 
-    try {
-        const category = await prisma.category.findFirst({
-            where: { name },
-            //@ts-ignore
-            include: { products: true },
-        });
+  try {
+    const category = await prisma.category.findFirst({
+      where: { name },
+      //@ts-ignore
+      include: { products: true },
+    });
 
-        res.send(category);
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+    res.send(category);
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
-app.post('/searchCategoriesByName', async (req, res) => {
-    const { name, page } = req.body;
+app.post("/searchCategoriesByName", async (req, res) => {
+  const { name, page } = req.body;
 
-    try {
-        const categories = await prisma.category.findMany({
-            where: {
-                name: { contains: name },
-            },
-            // @ts-ignore
-            include: { category: true },
-            skip: (page - 1) * 20,
-            take: 20,
-        });
+  try {
+    const categories = await prisma.category.findMany({
+      where: {
+        name: { contains: name },
+      },
+      // @ts-ignore
+      include: { category: true },
+      skip: (page - 1) * 20,
+      take: 20,
+    });
 
-        const count = await prisma.category.count({
-            where: {
-                name: { contains: name },
-            },
-        });
+    const count = await prisma.category.count({
+      where: {
+        name: { contains: name },
+      },
+    });
 
-        res.send({ categories, count });
-    } catch (err) {
-        // @ts-ignore
-        res.status(400).send({ error: err.message });
-    }
+    res.send({ categories, count });
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 
 // #endregion
